548b28e92933b3ca67adbab297dea8d0d1bf9a65
Jim Holmström <jim.holmstroem@gmail.com>
2014-08-14 07:42:58 +0200

    First try on product (alot like zip, except for it++ and another constructor signature for the iterators)

--- include/cit/product.h
+++ include/cit/product.h
@@ -1,5 +1,5 @@
-#ifndef _CITERTOOLS_ZIP_H_
-#define _CITERTOOLS_ZIP_H_
+#ifndef _CITERTOOLS_PRODUCT_H_
+#define _CITERTOOLS_PRODUCT_H_
 
 #include <cit/definition.h>
 #include <numeric>
@@ -14,42 +14,52 @@
 namespace cit2 {
 
     template<typename T, typename... Ts>
-    class _zip {
+    class _product {  //TODO alot of code is common with _zip
         const T & _t;
-        const _zip<Ts...> & _ts;
+        const _product<Ts...> & _ts;
       public:
         class const_iterator {
             typedef typename T::const_iterator inner_const_iterator;
-            typedef typename _zip<Ts...>::const_iterator outer_const_iterator;
-            const inner_const_iterator & _it;
+            typedef typename _product<Ts...>::const_iterator outer_const_iterator;
+            const inner_const_iterator & _it; //TODO need to copy at construction and have it modifiable for it++ to work as expected.
             const outer_const_iterator & _oit;
+            const outer_const_iterator & _oit_begin;
+            const outer_const_iterator & _oit_end;
+
           public:
-            typedef typename std::tuple<typename T::value_type, WHUT(_zip<Ts...>::value_type)...> value_type;
+            typedef typename std::tuple<typename T::value_type, WHUT(_product<Ts...>::value_type)...> value_type;
             const_iterator(const inner_const_iterator & it, const outer_const_iterator & oit) : _it(it), _iot(iot) {};
             bool operator==(const const_iterator & it) {
                 return it == _it || it._oit == _oit;
             };
             const_iterator operator++() {
-                _it++;
                 _oit++;
+                if _oit == _oit_end {
+                    _oit = _oit_begin; //TODO needs copyconstructor etc
+                    _it++;
+                } else {
+                    _oit++;
+                }
+
                 return *this;
             };
             value_type & operator*() const {  //NOTE dereference operator acts as "evaluate"
                 return std::tuple_cat(*_it, *_oit);
             };
         };
-        _zip(const T & t, const Ts &... ts) : _t(t), _ts(ts...) {};
+        _product(const T & t, const Ts &... ts) : _t(t), _ts(ts...) {};
         const_iterator begin() const {
-            return const_iterator(_t.begin(), _ts.begin());
+            return const_iterator(_t.begin(), _ts.begin(), _ts.begin(), _ts.end());
         };
         const_iterator end() const {
-            return const_iterator(_t.end(), _ts.end());
+            return const_iterator(_t.end(), _ts.end(), _ts.begin(), _ts.end());
         };
     };
 
     template<>
     template<typename T>
-    class _zip<T> {  //TODO the same as map(tuple<T>, ...)
+    class _product<T> {  //TODO the same as map(tuple<T>, ...)
+                         //TODO code duplication _zip<T>
         const T & _t;
       public:
         class const_iterator {
@@ -71,7 +81,7 @@ namespace cit2 {
             };
 
         };
-        _zip(const T & t) : _t(t) {};
+        _product(const T & t) : _t(t) {};
         const_iterator begin() const {
             return const_iterator(_t.begin());
         };
@@ -81,12 +91,12 @@ namespace cit2 {
     };
 
     template<typename T, typename... Ts>
-    _zip<T, Ts...> zip(const T & t, const Ts &... ts) {
-        return _zip<T, Ts...>(t, ts...);
+    _product<T, Ts...> product(const T & t, const Ts &... ts) {
+        return _product<T, Ts...>(t, ts...);
     };
 
 };
 
 #endif /* _CITERTOOLS_CPP11_ */
 
-#endif /* _CITERTOOLS_ZIP_H_ */
+#endif /* _CITERTOOLS_PRODUCT_H_ */
